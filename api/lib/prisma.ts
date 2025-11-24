d98bb8641719cdd9785c81c880f719c54f28d3a5
desibarra <desibarra@gmail.com>
2025-11-23 22:51:09 -0600

    Fix completo backend + serverless + imports + landing optimizada

--- api/lib/prisma.ts
+++ api/lib/prisma.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-declare const global: { prisma?: PrismaClient };
+export const prisma =
+    globalForPrisma.prisma || new PrismaClient();
 
-if (!global.prisma) {
-    global.prisma = new PrismaClient();
-}
-
-prisma = global.prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;

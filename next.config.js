aac37d73435d381eefcf2b6e5db1c44de9bc8bf6
desibarra <desibarra@gmail.com>
2025-11-24 20:09:39 -0600

    revert: Restore Vite + React Router configuration (working build)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-    experimental: {
-        serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
-    }
-}
-
-module.exports = nextConfig
